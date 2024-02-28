d6417bb362bff56319bc14c840c5f848d654f9c6
Vijay <30430062+vijaystudio45@users.noreply.github.com>
2024-02-28 12:26:47 +0530

    Update environment.js

--- src/environment.js
+++ src/environment.js
@@ -4,4 +4,4 @@ export const BACKEND_URL = "http://122.160.74.251:8041/";
 
 // export const Frontend_URL = "http://192.168.1.245:3023/";
 
-export const Frontend_URL = "http://122.160.74.251:3031/";
+export const Frontend_URL = "https://logohere45.onrender.com/";
